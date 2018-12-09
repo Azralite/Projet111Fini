439bcf36e212a9d2f1fbb4a39f8acd746eb71458
Paul Michel dit Ferrer <paul.mdf@icloud.com>
2018-12-09 16:05:03 +0100

    eau-total.cpp

---
+++ eau-total.cpp
@@ -0,0 +1,18 @@
+#include <stdexcept>
+/** @file **/
+#include <fstream>
+#include <iostream>
+using namespace std;
+/** Calcule et affiche le volume d'eau total distribué à Paris en 2011 **/
+int main() {
+ifstream volume;
+volume.open("donnees/volumes_d_eau_distribues.txt");
+	int mois;
+	int quantite;
+	int total = 0;
+	while(volume >> mois && volume >> quantite){
+		total = total + quantite;
+	}
+	cout << total << endl;
+	volume.close();
+}
