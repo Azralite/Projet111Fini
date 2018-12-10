721e2bae224811b8aa16ae1ca52215fe116a3673
Azralite <45258074+Azralite@users.noreply.github.com>
2018-12-10 14:45:09 +0100

    Update tableau-donnees.cpp

--- tableau-donnees.cpp
+++ tableau-donnees.cpp
@@ -3,6 +3,12 @@
 #include <fstream>
 #include "tableau-donnees.h"
 
+
+/** Renvoie la moyenne d'un tableau d'entier
+ * @param t, un tableau d'entiers
+ * @return la moyenne (entière) des valeurs
+ **/
+
 int moyenne(vector<int> t) {
     int moyenne = 0;
     for(int i=0;i< t.size(); i++){
@@ -12,6 +18,11 @@ int moyenne(vector<int> t) {
 	return moyenne;
 }
 
+/** Renvoie la somme d'un tableau d'entiers
+ * @param t, un tableau d'entier
+ * @return la somme des valeurs
+ **/
+
 int somme(vector<int> t) {
 	int total = 0;
 	for(int i=0;i< t.size(); i++){
@@ -20,11 +31,20 @@ int somme(vector<int> t) {
 	return total;
 }
 
+/** Somme partielle d'un tableau
+ * @param t un tableau d'entiers
+ * @result la somme des entiers à partir de l'indice i
+ **/
+
 int sommePartielle(vector<int> t, int i) {
     // Remplacer la ligne suivante par le code adéquat
     throw runtime_error("Fonction sommePartielle non implanté ligne 18");
 }
 
+/** Renvoie l'indice de la valeur maximale du tableau
+ * @param t, un tableau d'entier
+ * @return l'indice de la valeur maximale ou -1 si le tableau est vide
+ **/
 
 int indiceMax(vector<int> t) {
 	int valeurMax = 0;
@@ -41,10 +61,24 @@ int indiceMax(vector<int> t) {
 	return rangMax;
 }
 
+/** Construction d'un tableau 2D d'entiers lu depuis un fichier
+ * @param fichier le nom d'un fichier contenant un nombre fixe
+  * d'entiers par lignes, séparés par des espaces
+ * @param nb_colonnes le nombre de colonnes du fichier
+ * @return un tableau d'entiers à deux dimensions
+ **/
+
 vector<vector<int>> litTableauInt(string nom_fichier, int nb_colonnes) {
     // Remplacer la ligne suivante par le code adéquat
     throw runtime_error("Fonction litTableauInt non implanté ligne 29");
 }
+
+
+/** Extraction d'une colonne d'un tableau d'entiers
+ * @param t un tableau 2D d'entiers
+ * @param j un numéro de colonne
+ * @return la colonne j, représentée par un vecteur d'entiers
+ **/
 
 vector<int> colonne(vector<vector<int>> t, int i) {
     // Remplacer la ligne suivante par le code adéquat
